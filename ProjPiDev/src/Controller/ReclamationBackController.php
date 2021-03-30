af3f805564edddc4ae6018f59760972fcc93b343
Khalil <khalil.azizi@esprit.tn>
2021-03-31 00:22:08 +0100

    Status ADDED functional

--- ProjPiDev/src/Controller/ReclamationBackController.php
+++ ProjPiDev/src/Controller/ReclamationBackController.php
@@ -43,39 +43,72 @@ class ReclamationBackController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/back/reclamation/delete/{id}", name="delete_back_reclamation")
-     */
-    public function Delete($id)
-    {
-        $em=$this->getDoctrine()->getManager();
-        $reclamation=$em->getRepository(Reclamation::class)->find($id);
-        $em->remove($reclamation);
-        $em->flush();
-        return $this->redirectToRoute("affiche_back_Reclamation");
-    }
+//    /**
+//     * @Route("/back/reclamation/delete/{id}", name="delete_back_reclamation")
+//     */
+//    public function Delete($id)
+//    {
+//        $em=$this->getDoctrine()->getManager();
+//        $reclamation=$em->getRepository(Reclamation::class)->find($id);
+//        $em->remove($reclamation);
+//        $em->flush();
+//        return $this->redirectToRoute("affiche_back_Reclamation");
+//    }
+
     /**
      * @Route("/back/reclamation/approve/{id}", name="approve")
      */
-    public function Approve ($id): Response
+    public function Approve ($id , \Swift_Mailer $mailer): Response
     {
+        $em = $this->getDoctrine()->getRepository(Reclamation::class);
+        $reclamation = $em->find($id);
+
         $repo = $this->getDoctrine()->getRepository(Reclamation::class);
         $etat = $repo->find($id);
         $etat->setStatus('Résolu');
         $em = $this->getDoctrine()->getManager();
         $em->flush();
+
+        $message = (new \Swift_Message('Votre Réclamation est Résolu'))
+            ->setFrom('pidevtestad@gmail.com')
+            ->setTo('khalil.azizi@esprit.tn')
+            ->setBody(
+                $this->renderView(
+                    'email/emailReclamation.html.twig' , ["reclamation"=>$reclamation]
+                ),
+                'text/html'
+            );
+        $mailer->send($message);
+
         return $this->redirectToRoute('affiche_back_Reclamation',array('id'=>$etat->getId()));
     }
     /**
      * @Route("/back/reclamation/disapprove/{id}", name="disapprove")
      */
-    public function Disapprove ($id): Response
+    public function Disapprove ($id , \Swift_Mailer $mailer): Response
     {
+        $em = $this->getDoctrine()->getRepository(Reclamation::class);
+        $reclamation = $em->find($id);
+
         $repo = $this->getDoctrine()->getRepository(Reclamation::class);
         $etat = $repo->find($id);
         $etat->setStatus('Non Résolu');
         $em = $this->getDoctrine()->getManager();
         $em->flush();
+        $message = (new \Swift_Message('Votre Réclamation est Non Résolu'))
+            ->setFrom('pidevtestad@gmail.com')
+            ->setTo('khalil.azizi@esprit.tn')
+            ->setBody(
+                $this->renderView(
+                    'email/emailReclamation.html.twig' , ["reclamation"=>$reclamation]
+                ),
+                'text/html'
+            );
+        $mailer->send($message);
+
+        $em->remove($reclamation);
+        $em->flush();
+
         return $this->redirectToRoute('affiche_back_Reclamation',array('id'=>$etat->getId()));
     }
 }
